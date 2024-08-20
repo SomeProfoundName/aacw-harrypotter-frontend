e2ff3862227a028c769ded410fa7867a5331fc49
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-08-20 17:28:23 +0100

    add back of card info

--- script.js
+++ script.js
@@ -14,45 +14,58 @@ searchInput.addEventListener("input", (e) => {
   });
 });
 
-// getCharacters();
+getCharacters();
 
-// function getCharacters() {
-//   fetch("https://aacw-harrypotter-backend-api.onrender.com/characters")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//       showCharacters(data);
-//     });
-// }
+function getCharacters() {
+  fetch("https://aacw-harrypotter-backend-api.onrender.com/characters")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      showCharacters(data);
+    });
+}
 
-// function showCharacters(data) {
-//   main.innerHTML = "";
+function showCharacters(data) {
+  main.innerHTML = "";
 
-//   characterArray = data.map((character) => {
-//     const { name, image } = character;
-//     const characterEl = document.createElement("div");
-//     characterEl.classList.add("character");
+  characterArray = data.map((character) => {
+    const { name, image, alternate_names, species, gender, house, dateOfBirth, wiki } = character;
+    const characterEl = document.createElement("div");
+    characterEl.classList.add("character");
 
-//     characterEl.innerHTML = `
-//         <div class="character-inner">
-//             <div class="front">
-//                 <img src="${image}" alt="a picture of a ${name}">
-//                 <div class="character-info">
-//                     <h3>${name}</h3>
-//                 </div>
-//                 </div>
-//                 <div class="back">
-//                 <h3>Back of the card</h3>
-//                 </div>
-//             </div>
-//         </div>
-//         `;
+    characterEl.innerHTML = `
+<div class="character-inner">
+            <div class="front">
+              <img src=${image} alt="a picture of a wizard" />
+              <div class="character-info">
+                <h3>${name}</h3>
+              </div>
+            </div>
+            <div class="back">
+              <h2>${name}</h2>
+              <h3>Alt Names:</h3>
+              <p>${alternate_names}</p>
+              <h3>Species:</h3>
+              <p>${species}</p>
+              <h3>Gender:</h3>
+              <p>${gender}</p>
+              <h3>House:</h3>
+              <p>${house}</p>
+              <h3>Date of Birth:</h3>
+              <p>${dateOfBirth}</p>
 
-//     main.appendChild(characterEl);
-//     return { name: character.name, element: characterEl };
-//   });
-// }
+              <span id="learn"><a href="${wiki}" target="_blank">Learn More ></a></span>
 
-// function handleError(imgElement) {
-//   imgElement.src = "images/wizard-icon.jpeg";
-// }
+            </div>
+          </div>
+        </div>
+        `;
+
+    main.appendChild(characterEl);
+    return { name: character.name, element: characterEl };
+  });
+}
+
+function handleError(imgElement) {
+  imgElement.src = "images/wizard-icon.jpeg";
+}
