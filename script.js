8170a49c751154df1aaf955a932a09e32eba7efa
Collins <84730393+SomeProfoundName@users.noreply.github.com>
2024-08-20 16:37:47 +0100

    modify card back info, add styling

--- script.js
+++ script.js
@@ -14,45 +14,45 @@ searchInput.addEventListener("input", (e) => {
   });
 });
 
-getCharacters();
+// getCharacters();
 
-function getCharacters() {
-  fetch("https://aacw-harrypotter-backend-api.onrender.com/characters")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      showCharacters(data);
-    });
-}
+// function getCharacters() {
+//   fetch("https://aacw-harrypotter-backend-api.onrender.com/characters")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//       showCharacters(data);
+//     });
+// }
 
-function showCharacters(data) {
-  main.innerHTML = "";
+// function showCharacters(data) {
+//   main.innerHTML = "";
 
-  characterArray = data.map((character) => {
-    const { name, image } = character;
-    const characterEl = document.createElement("div");
-    characterEl.classList.add("character");
+//   characterArray = data.map((character) => {
+//     const { name, image } = character;
+//     const characterEl = document.createElement("div");
+//     characterEl.classList.add("character");
 
-    characterEl.innerHTML = `
-        <div class="character-inner">
-            <div class="front">
-                <img src="${image}" alt="a picture of a ${name}">
-                <div class="character-info">
-                    <h3>${name}</h3>
-                </div>
-                </div>
-                <div class="back">
-                <h3>Back of the card</h3>
-                </div>
-            </div>
-        </div>
-        `;
+//     characterEl.innerHTML = `
+//         <div class="character-inner">
+//             <div class="front">
+//                 <img src="${image}" alt="a picture of a ${name}">
+//                 <div class="character-info">
+//                     <h3>${name}</h3>
+//                 </div>
+//                 </div>
+//                 <div class="back">
+//                 <h3>Back of the card</h3>
+//                 </div>
+//             </div>
+//         </div>
+//         `;
 
-    main.appendChild(characterEl);
-    return { name: character.name, element: characterEl };
-  });
-}
+//     main.appendChild(characterEl);
+//     return { name: character.name, element: characterEl };
+//   });
+// }
 
-function handleError(imgElement) {
-  imgElement.src = "images/wizard-icon.jpeg";
-}
+// function handleError(imgElement) {
+//   imgElement.src = "images/wizard-icon.jpeg";
+// }
